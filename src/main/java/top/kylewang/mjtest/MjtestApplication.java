2a34309bba918263a4663fd4d9a74c44e7b11cf9
KyleWang <wk52525@126.com>
2018-04-23 16:13:56 +0800

    init

---
+++ src/main/java/top/kylewang/mjtest/MjtestApplication.java
@@ -0,0 +1,12 @@
+package top.kylewang.mjtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjtestApplication.class, args);
+	}
+}
