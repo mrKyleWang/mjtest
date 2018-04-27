020ccdc178fd0038157c6ff080f12777207d7a73
KyleWang <wk52525@126.com>
2018-04-27 17:52:33 +0800

    cpu collect fix

--- src/test/java/top/kylewang/mjtest/MjtestApplicationTests.java
+++ src/test/java/top/kylewang/mjtest/MjtestApplicationTests.java
@@ -1,13 +1,11 @@
 package top.kylewang.mjtest;
 
+import com.sun.management.OperatingSystemMXBean;
 import org.junit.Test;
-import top.kylewang.mjtest.collect.cpu.CpuCollector;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.management.ManagementFactory;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+
 public class MjtestApplicationTests {
 
 	@Test
@@ -19,32 +17,18 @@ public class MjtestApplicationTests {
 	}
 
 	@Test
-	public void test() {
-		String s = "%Cpu(s):  6.2 us,  6.2 sy,  0.0 ni, 87.5 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st";
-		double[] doubles = CpuCollector.resultConvert(s);
-		for (double aDouble : doubles) {
-			System.out.println(aDouble);
-		}
-	}
+	public void cpuTest(){
+		OperatingSystemMXBean bean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
 
-	@Test
-	public void test2() {
-		String source = "Cpu(s): 12.3%us,  0.4%sy,  0.0%ni, 86.9%id,  0.2%wa,  0.0%hi,  0.0%si,  0.0%st";
-		String[] strings = source.split(",");
-		for (String string : strings) {
-			String regex = "\\d+.\\d+";
-			System.out.println(getMatcher(regex, string));
+		while (true){
+			System.out.println(bean.getSystemCpuLoad()*100+"%");
+			System.out.println(bean.getProcessCpuLoad()*100+"%");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-
-	public String getMatcher(String regex, String source) {
-		String result = "";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(source);
-		while (matcher.find()) {
-			result = matcher.group();
-		}
-		return result;
 	}
 
 }
