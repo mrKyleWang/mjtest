d2ebd373d6b92d4b89b3b95c4740270672d4394f
KyleWang <wk52525@126.com>
2018-04-28 13:20:02 +0800

    fix

--- src/test/java/top/kylewang/mjtest/MjtestApplicationTests.java
+++ src/test/java/top/kylewang/mjtest/MjtestApplicationTests.java
@@ -4,7 +4,8 @@ import com.sun.management.OperatingSystemMXBean;
 import org.junit.Test;
 
 import java.lang.management.ManagementFactory;
-
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
 
 public class MjtestApplicationTests {
 
@@ -17,17 +18,46 @@ public class MjtestApplicationTests {
 	}
 
 	@Test
-	public void cpuTest(){
-		OperatingSystemMXBean bean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+	public void cpuTest() {
+		OperatingSystemMXBean bean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 
-		while (true){
-			System.out.println(bean.getSystemCpuLoad()*100+"%");
-			System.out.println(bean.getProcessCpuLoad()*100+"%");
+		while (true) {
+			System.out.println(bean.getSystemCpuLoad() * 100 + "%");
+			System.out.println(bean.getProcessCpuLoad() * 100 + "%");
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	public void formatTest(Double source) {
+		double d = 1.717557251908397;
+		System.out.println((double) Math.round(source * 10000) / 10000);
+	}
+
+	public void formatTest2(Double source) {
+		BigDecimal bg = new BigDecimal(source);
+		System.out.println(bg.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue());
+	}
+
+	@Test
+	public void formatTimeTest() {
+		double d = 0.38167938931297707;
+		long l1 = System.currentTimeMillis();
+		formatTest(d);
+		long l2 = System.currentTimeMillis();
+		System.out.println(l2 - l1);
+	}
+
+	@Test
+	public void stringTokenizerTest(){
+		String s = new String("This is a test string");
+		StringTokenizer st = new StringTokenizer(s);
+		System.out.println( "Token Total: " + st.countTokens() );
+		while( st.hasMoreElements() ){
+			System.out.println(st.nextToken());
 		}
 	}
 
